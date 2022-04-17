499d4b75c9754c909d82fbe0dd94087c451ad881
zwm-Future <871858554@qq.com>
2022-04-17 21:18:17 +0800

    add end-btn

---
+++ components/end-btn/end-btn.js
@@ -0,0 +1,41 @@
+// components/end-btn/end-btn.js
+let timer = null;
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    className: ''
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleLongpress:function () {
+      const className = 'animation'
+      timer = setTimeout(() => {
+        console.log(111);
+      },1000)
+      this.setData({
+        className
+      })
+      
+    },
+    handleTouchEnd:function () {
+      clearTimeout(timer);
+      this.setData(
+       {
+         className: ''
+       }
+      )
+    }
+  }
+})
