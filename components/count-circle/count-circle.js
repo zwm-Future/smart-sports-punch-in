39be22394b62360a38747376fcd534cf31802690
zwm-Future <871858554@qq.com>
2022-04-20 23:57:42 +0800

    add event

--- components/count-circle/count-circle.js
+++ components/count-circle/count-circle.js
@@ -1,4 +1,5 @@
-// components/count-circle/count-circle.js
+let count = 0;
+let timer = null;
 Component({
   /**
    * 组件的属性列表
@@ -11,13 +12,47 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    times:'00:00',
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    start:function () {
+      count = 0;
+      clearInterval(timer);
+      timer = setInterval(() => {
+        count++;
+        const times = this.countToTimes(count);
+        this.setData({
+          times
+        })
+      },1000)
+    },
+    stop:function () {
+      clearInterval(timer);
+    },
+    continue:function () {
+      clearInterval(timer);
+      timer = setInterval(() => {
+        count++;
+        const times = this.countToTimes(count);
+        this.setData({
+          times
+        })
+      },1000)
+    },
+    getTimes:function () {
+      return this.data.times;
+    }
+    ,
+    countToTimes:function (count) {
+      let seconds = count % 60;
+      let minutes = parseInt(count / 60);
+      seconds = seconds < 10 ?  '0' + seconds : seconds;
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      return minutes + ':' + seconds;
+    }
   }
 })
--- pages/punch/operation-tab/operation-tab.js
+++ pages/punch/operation-tab/operation-tab.js
@@ -18,6 +18,11 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    hanlePause:function () {
+      console.log('press pause');
+    },
+    handleContinue:function () {
+      console.log('press continue');
+    }
   }
 })
