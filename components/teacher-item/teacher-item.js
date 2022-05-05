789aed644ebdc2b1681d6a8ea45a052071458a14
zwm-Future <871858554@qq.com>
2022-05-05 23:49:36 +0800

    add select-teacher page

---
+++ components/teacher-item/teacher-item.js
@@ -0,0 +1,30 @@
+// components/teacher-item/teacher-item.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name:{
+      type:"String",
+      value:"老师姓名"
+    },
+    number:{
+      type:"String",
+      value:"xxxxxxxxx"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
