91bb04b0a4106e7e23316df7540108fe89d5410a
zwm-Future <871858554@qq.com>
2022-05-06 18:17:05 +0800

    modify accumulate-component

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,25 +4,11 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    
   },
   onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+    const app = getApp();
+    console.log(app.globalData);
   },
   onShow: function () {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
