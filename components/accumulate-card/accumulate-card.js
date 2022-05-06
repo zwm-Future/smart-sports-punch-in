f5b7f2f16d265c81b2aaab01c60f0f937dd0ffe8
zwm-Future <871858554@qq.com>
2022-05-06 18:08:08 +0800

    add data

--- components/accumulate-card/accumulate-card.js
+++ components/accumulate-card/accumulate-card.js
@@ -1,4 +1,6 @@
-// components/accumulate-card/accumulate-card.js
+import {
+  getsportRecordTotal
+} from "../../api/student/sports"
 Component({
   /**
    * 组件的属性列表
@@ -11,13 +13,43 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    minutes: 0,
+    score: 0
   },
-
+  lifetimes: {
+    ready: function () {
+      this.getTotalRecord()
+    }
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    getTotalRecord: async function () {
+      try {
+        const app = getApp();
+        const {
+          user
+        } = app.globalData;
+        const res = await getsportRecordTotal({
+          semesterId: 1,
+          stuId: user.id
+        });
+        const {
+          code,
+          data
+        } = res;
+        if (code) {
+          const minutes = data.sportTime == null ? 0 : parseInt(data.sportTime / 60);
+          const score = data.score == null ? 0 : data.score;
+          this.setData({
+            minutes,
+            score
+          })
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 })
