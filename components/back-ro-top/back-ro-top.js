55d81d0051bda1ced0887ef6cfed45a711551389
zwm-Future <871858554@qq.com>
2022-05-06 18:12:55 +0800

    new Compoents

---
+++ components/back-ro-top/back-ro-top.js
@@ -0,0 +1,28 @@
+// components/back-ro-top/back-ro-top.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleToTOp: function () {
+      wx.pageScrollTo({
+        scrollTop: 0,
+        duration: 300
+      })
+    }
+  }
+})
---
+++ components/logout-mask/logout-mask.js
@@ -0,0 +1,46 @@
+// components/show-modal/show-modal.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    logoutModalVisible: {
+      type: Boolean,
+      value: false,
+      observer: '_visibleChange',
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    data: {
+      visible: false,
+    },
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    _visibleChange: function (newVal, oldVal, changedPath) {
+      if (oldVal === false && newVal === true) {
+        setTimeout(function () {
+          this._onShow();
+        }.bind(this), 0)
+      }
+    },
+    _onShow: function () {
+      this.setData({
+        visible: true,
+      })
+    },
+    _onCancel: function () {
+      this.setData({
+        visible: false,
+      })
+      this.triggerEvent('modalCancel');
+    }
+  }
+})
