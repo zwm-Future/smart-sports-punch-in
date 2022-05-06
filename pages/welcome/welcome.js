d663845185aae26cb0536b36c37a24cb5409809a
zwm-Future <871858554@qq.com>
2022-05-06 18:15:24 +0800

    modify wxml

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,19 +1,40 @@
 Page({
   data: {
-    showAuth:false
+    showAuth: false,
   },
   onLoad: function () {
-    wx.getSystemInfo({
-      success: (result) => {
-        console.log(result);
-      },
-    })
-    wx.getUserInfo({
-      withCredentials:true,
-      lang:'zh_CN',
-      success:(res) => {
-        console.log(res);
-      }
+    //Prodcut -> 放开 this.product 注释 wx.getShareInfo({
+    this.product();
+    // wx.getUserInfo({
+    //   withCredentials: true,
+    //   lang: 'zh_CN',
+    //   success: (res) => {
+    //     if(res.encryptedData && res.iv) {
+    //       wx.switchTab({
+    //         url: '/pages/index/index',
+    //       })
+    //     }
+    //   }
+    // })
+  },
+  handleShowAuth: function () {
+    this.setData({
+      showAuth: true
     })
   },
+  //Product Login
+  product: function () {
+    const app = getApp()
+    wx.request({
+      url: app.globalData.baseUrl + '/user/loginPsw?number=123',
+      success: (res) => {
+        const {code,data} = res.data;
+        const app = getApp()
+        if(code) app.globalData.user = data;
+        wx.switchTab({
+          url: '/pages/index/index',
+        })
+      }
+    })
+  }
 })
