b9ec390d676bb3a1c7e6bb7efae504e19fbd0462
zwm-Future <871858554@qq.com>
2022-05-06 18:17:25 +0800

    fix

--- api/request.js
+++ api/request.js
@@ -1,7 +1,6 @@
 const app = getApp();
 const baseURL = app.globalData.baseUrl;
 const showTip = require('../public/showTip');
-console.log(baseURL);
 export default function Request(options) {
   return new Promise((resolve, reject) => {
     showTip.Loading(1);
@@ -9,13 +8,12 @@ export default function Request(options) {
       url: baseURL + options.url || '',
       data: options.data || {},
       method: options.method || 'GET',
-      header: options.header || {
-        'content-type': "application/x-www-form-urlencoded"
+      header: {
+        "Access-Control-Allow-Origin": "*",
       },
       responseType: options.responseType || "",
       timeout: 15000,
       success(res) {
-        console.log(res);
         showTip.LoadingOff();
         if (res.statusCode === 200) {
           resolve(res.data);
@@ -23,16 +21,17 @@ export default function Request(options) {
           const {
             statusCode
           } = res;
-          if (statusCode == 404) reject('服务器出问题了，请稍后重试！')
+          if (statusCode == 404) {
+            res.mes = '服务器出问题了，请稍后重试！'
+            reject(res)
+          }
         };
       },
       fail(res) {
+        showTip.LoadingOff();
         showTip.Toast("网络开小差了");
         reject(res);
       },
-      complete() {
-        showTip.LoadingOff();
-      }
     })
   })
 };
