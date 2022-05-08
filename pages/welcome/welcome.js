031b94b42ad30367fd86afee31de78ae4eccedfe
zwm-Future <871858554@qq.com>
2022-05-08 20:11:54 +0800

    add login auth and authFail

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -2,39 +2,30 @@ Page({
   data: {
     showAuth: false,
   },
-  onLoad: function () {
-    //Prodcut -> 放开 this.product 注释 wx.getShareInfo({
-    this.product();
-    // wx.getUserInfo({
-    //   withCredentials: true,
-    //   lang: 'zh_CN',
-    //   success: (res) => {
-    //     if(res.encryptedData && res.iv) {
-    //       wx.switchTab({
-    //         url: '/pages/index/index',
-    //       })
-    //     }
-    //   }
-    // })
+  onLoad: async function () {
+    try {
+      await wx.checkSession();
+      wx.switchTab({
+        url: '/pages/index/index',
+      })
+    } catch (error) {
+      console.log(error);
+    }
   },
   handleShowAuth: function () {
     this.setData({
       showAuth: true
     })
   },
-  //Product Login
-  product: function () {
-    const app = getApp()
-    wx.request({
-      url: app.globalData.baseUrl + '/user/loginPsw?number=123',
-      success: (res) => {
-        const {code,data} = res.data;
-        const app = getApp()
-        if(code) app.globalData.user = data;
-        wx.switchTab({
-          url: '/pages/index/index',
-        })
-      }
+  authFail: function (e) {
+    console.log('authFail');
+    this.setData({
+      showAuth: false
+    }, () => {
+      wx.showToast({
+        title: e.detail ? e.detail : '授权失败',
+        icon: 'error'
+      })
     })
   }
 })
