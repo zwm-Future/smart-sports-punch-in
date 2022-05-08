4201ca297b70a9b1636c237ab4c019e2f7b14514
zwm-Future <871858554@qq.com>
2022-05-08 20:11:17 +0800

    add login auth

--- components/auth-dialog/auth-dialog.js
+++ components/auth-dialog/auth-dialog.js
@@ -1,4 +1,9 @@
-// components/auth-dialog/auth-dialog.js
+import {
+  login
+} from '../../api/login.js'
+import {
+  loginProduct
+} from '../../api/student/login'
 Component({
   /**
    * 组件的属性列表
@@ -7,50 +12,73 @@ Component({
     show: {
       type: Boolean,
       value: false
-    }
-  },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-    logoutModalVisible:false
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    handdleTap: function (e) {
-      const {
-        type
-      } = e.target.dataset;
-      if (type == 'logout') {
-        console.log('logout');
-        this.setData({
-          logoutModalVisible:true
-        })
-      } else {
-        console.log(11);
-        wx.getSetting({
-          success(res) {
-            console.log(res);
-            if (!res.authSetting['scope.userInfo']) {
-              wx.authorize({
-                scope: 'scope.userInfo',
-                success(res) {
-                  console.log(res);
-                }
-              })
-            }
-          }
-        })
-      }
     },
+  },
+  data: {
+    logoutModalVisible: false,
+    disable: false
+  },
+  methods: {
+    handdleTap: async function (e) {
+      try {
+        this.setData({
+          disable: true
+        });
+        const {
+          type
+        } = e.target.dataset;
+        //退出小程序
+        if (type == 'logout') {
+          console.log('logout');
+          this.setData({
+            logoutModalVisible: true
+          })
+        } else { //登录授权
+          // const {
+          //   userInfo
+          // } = await wx.getUserProfile({
+          //   desc: '展示用户信息',
+          // })
+          // //成功授权
+          // userInfo && wx.setStorageSync('userInfo', userInfo);
+          //登录验证
+          this.handleLogin();
+        }
+      } catch (error) {
+        //用户信息授权失败
+        this.triggerEvent('handleAuthFail', '授权失败');
+      }
+      this.setData({
+        disable: false
+      });
+    },
+    //登录验证
     handleCancleModal: function () {
       this.setData({
         logoutModalVisible: false
       })
     },
+    handleLogin: async function () {
+      try {
+        const {
+          code
+        } = await wx.login({});
+        console.log(code);
+        // const res = await login(code)
+        //Product
+        const {
+          code: loginCode,
+          data
+        } = await loginProduct();
+        if (loginCode) {
+          wx.setStorageSync("user", data);
+          wx.switchTab({
+            url: '/pages/index/index',
+          })
+        }
+      } catch (error) {
+        console.log('login', error);
+      }
+    }
   },
 })
