381c8c21e697986609c7879a44fee606671a285a
zwm-Future <871858554@qq.com>
2022-05-08 20:13:51 +0800

    add voice

--- components/wave_bg/wave_bg.js
+++ components/wave_bg/wave_bg.js
@@ -4,14 +4,14 @@ Component({
    * 组件的属性列表
    */
   properties: {
-  
+
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    show:false,
+    show: false,
     count: 3,
     animationData: {}
   },
@@ -22,9 +22,10 @@ Component({
   methods: {
     _handleCount: function (callBack) {
       this.setData({
-        show:true,
-        count:3
+        show: true,
+        count: 3
       })
+      this._playVoice()
       this.timer = setInterval(() => {
         let {
           count
@@ -33,7 +34,7 @@ Component({
           clearInterval(this.timer);
           this.timer = null;
           this.setData({
-            show:false
+            show: false
           })
           callBack && callBack();
           return
@@ -43,6 +44,15 @@ Component({
           count
         })
       }, 1000);
+    },
+    _playVoice: function () {
+      const innerAudioContext = wx.createInnerAudioContext();
+      innerAudioContext.onError(res => {
+        console.log(res);
+      })
+      innerAudioContext.src = '/public/voice/count_down_zh_CN.mp3';
+      innerAudioContext.playbackRate = 1.2
+      innerAudioContext.play();
     },
   },
   lifetimes: {
