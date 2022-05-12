ae8d823c78dcc431a58a7e47794cabfd9be8cc3f
zwm-Future <871858554@qq.com>
2022-05-12 18:28:34 +0800

    improve

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,11 +1,14 @@
-// pages/my/my.js
+import {
+  getMyTeacher
+} from '../../api/student/my-teacher'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    teacherName: ''
   },
 
   /**
@@ -19,7 +22,10 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    const userInfo = wx.getStorageSync('userInfo');
+    this.setData({
+      userInfo
+    });
   },
 
   /**
@@ -31,40 +37,47 @@ Page({
         selected: 2
       })
     }
+    this._setTeacher();
   },
-
+  _setTeacher: function () {
+    try {
+      let myTeacher = wx.getStorageSync('myTeacher');
+      if (!myTeacher) myTeacher = this._getTeaher();
+      this.setData({
+        teacherName: myTeacher.teacherName
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  _getTeaher: async function () {
+    try {
+      const {
+        code,
+        data,
+        other
+      } = await getMyTeacher({
+        semesterId: '1'
+      })
+      const teacherName = data.name ? data.name : '';
+      const courseInfo = other && other.name ? other.name : '';
+      code && wx.setStorageSync('myTeacher', {
+        teacherName,
+        courseInfo
+      })
+      return {
+        teacherName,
+        courseInfo
+      };
+    } catch (error) {
+      console.log(error);
+      error.mes && Toast(error.mes, 'none');
+    }
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
