3e6388e918c9c9c062e97066c5e0cdbf0d2251dc
zwm-Future <871858554@qq.com>
2022-05-29 13:41:53 +0800

    improve

--- components/puzzle-verify/puzzle-verify.js
+++ components/puzzle-verify/puzzle-verify.js
@@ -14,18 +14,25 @@ Component({
     dragLeft: 0,
     statusClass: ''
   },
+  observers: {
+    'show': function (value) {
+      if (value) {
+        let _this = this;
+        this.createSelectorQuery()
+          .select("#vf")
+          .context(function (res) {
+            _this.canvasBox = res.context;
+            _this.initCanvas();
+          })
+          .exec();
+      }
+    }
+  },
   finalX: '0',
   canvasBox: null,
   lifetimes: {
     attached: function () {
-      let _this = this;
-      this.createSelectorQuery()
-        .select("#vf")
-        .context(function (res) {
-          _this.canvasBox = res.context;
-          _this.initCanvas()
-        })
-        .exec();
+
     }
   },
   methods: {
@@ -56,13 +63,18 @@ Component({
       wx.getSystemInfo({
         success: function (res) {
           rpx = res.windowWidth / 375;
-          console.log(rpx);
         },
       })
       const canvasHeight = 150 * rpx
       this.setData({
         canvasHeight
       })
+      // wx.downloadFile({
+      //   url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fnewsletter.teldap.tw%2Ffile%2Ffile%2F75%2F7599.jpg&refer=http%3A%2F%2Fnewsletter.teldap.tw&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1655011791&t=d5dac5b6cdd5dc45b3e1ca1d9dd889d0',
+      //   success:(res) => {
+      //     console.log(res);
+      //   }
+      // })
       this.canvasBox.drawImage('/img/index/src=http___newsletter.teldap.tw_file_file_75_7599.jpg&refer=http___newsletter.teldap.webp', 0, 0, 300 * rpx, canvasHeight)
       const l = 50 * rpx;
       const left = 150 * rpx;
