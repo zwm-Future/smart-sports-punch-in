366384271f4de9b04eea89353e14bfb6e25d2d38
zwm-Future <871858554@qq.com>
2022-05-31 16:44:52 +0800

    newAdd sport-item-card Compoent

---
+++ components/sport-item-card/sport-item-card.js
@@ -0,0 +1,26 @@
+// components/sport-item-card/sport-item-card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    sport:{
+      type:Object,
+      value:{}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
