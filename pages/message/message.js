e58299c89676a24013e638688609a0ba72b5c7fb
zwm-Future <871858554@qq.com>
2022-05-31 16:45:57 +0800

    api->data

--- pages/message/message.js
+++ pages/message/message.js
@@ -1,66 +1,45 @@
-// pages/message/message.js
+import {
+  getMessage
+} from '../../api/student/message'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    courseMes: {
+      unReadSize: 0,
+      date: ''
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
+    this._getMes()
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  _getMes: async function () {
+    try {
+      const {
+        code,
+        data,
+        other: {
+          unReadSize
+        }
+      } = await getMessage();
+      if (code) {
+        let earlistIndex = 0;
+        //处获取最新消息时间
+        let t = new Date(data[0].createStamp);
+        data.forEach((item, index) => {
+          const date = new Date(item.createStamp)
+          if (data > t) {
+            t = data, earlistIndex = index
+          }
+        })
+        this.setData({
+          courseMes: {
+            unReadSize,
+            date: data[earlistIndex].createStamp
+          }
+        })
+      }
+    } catch (error) {
+      console.log(error);
+    }
 
   }
 })
