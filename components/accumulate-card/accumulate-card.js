ac94090433a00acb190b5f23638863ea4c5fd41e
zwm-Future <871858554@qq.com>
2022-06-01 16:15:06 +0800

    f:data -> son

--- components/accumulate-card/accumulate-card.js
+++ components/accumulate-card/accumulate-card.js
@@ -1,52 +1,12 @@
-import {
-  getsportRecordTotal
-} from "../../api/student/sports"
 Component({
-  /**
-   * 组件的属性列表
-   */
   properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-    minutes: 0,
-    score: 0
-  },
-  lifetimes: {
-    ready: function () {
-      this.getTotalRecord()
+    minutes: {
+      type: Number,
+      value: 0,
+    },
+    score: {
+      type: Number,
+      value: 0,
     }
   },
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    getTotalRecord: async function () {
-      try {
-        const user = wx.getStorageSync('user')
-        const res = await getsportRecordTotal({
-          semesterId: 1,
-          stuId: user.id
-        });
-        const {
-          code,
-          data
-        } = res;
-        if (code) {
-          const minutes = data.sportTime == null ? 0 : parseInt(data.sportTime / 60);
-          const score = data.score == null ? 0 : data.score;
-          this.setData({
-            minutes,
-            score
-          })
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
 })
--- components/today-done-card/today-done-card.js
+++ components/today-done-card/today-done-card.js
@@ -1,34 +1,15 @@
-import {
-  getTodaysportRecord
-} from '../../api/student/sports.js'
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
-  },
-  data: {
-    sportTime:0,
-    score:0
-  },
-  pageLifetimes:{
-    show:function() {
-      this.getTodayRecord();
+    sportTime: {
+      type: Number,
+      value: 0,
+    },
+    score: {
+      type: Number,
+      value: 0,
     }
   },
-  methods: {
-    getTodayRecord: async function () {
-      try {
-        const user = wx.getStorageSync('user')
-        const {code,data} = await getTodaysportRecord({
-          userId: user.id
-        });
-        code && this.setData({sportTime:data.sportTime,score:data.score})
-        console.log(res);
-      } catch (error) {
-
-      }
-    }
-  }
 })
