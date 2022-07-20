1b438f1a5907340c2508fe30f29fc8359d0cb755
zwm-Future <871858554@qq.com>
2022-07-20 21:06:54 +0800

    消息发布接口

--- api/teacher/message.js
+++ api/teacher/message.js
@@ -1,13 +1,19 @@
 import Request from '../request';
 
-export const addCourseMes = ({content,courseId}) => {
+export const addCourseMes = ({
+  content,
+  courseId
+}) => {
   /** 新增课程消息
    * @param {String} content - 消息内容
    * @param {Arr} [courseId] - 课程id
    */
-    return Request({
-      url:`/msg/course?content=${content}`,
-      method:'POST',
-      data:courseId
-    })
+  return Request({
+    url: '/msg/course',
+    method: 'POST',
+    data: {
+      content,
+      courseId
+    }
+  })
 }
