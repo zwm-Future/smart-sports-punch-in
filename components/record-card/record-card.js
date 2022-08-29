3641123577790e044bdc37132e51e67dbc6a1cbd
zwm-Future <871858554@qq.com>
2022-08-29 21:12:08 +0800

    图片->在线获取

--- components/record-card/record-card.js
+++ components/record-card/record-card.js
@@ -1,34 +1,36 @@
 // components/record-card/record-card.js
+const app = getApp();
+const baseURL = app.globalData.baseUrl;
 //name --> color
 const colorClass = {
-  '篮球':'basketball-color',
-  '足球':'football-color',
-  '网球':'tennis-color',
-  '排球':'volley-color',
-  '羽毛球':'badminton-color'
+  '篮球': 'basketball-color',
+  '足球': 'football-color',
+  '网球': 'tennis-color',
+  '排球': 'volley-color',
+  '羽毛球': 'badminton-color'
 }
 //name --> imgClass
 const imgClass = {
-  '篮球':'basketball-img',
-  '足球':'football-img',
-  '网球':'tennis-img',
-  '排球':'volley-img',
-  '羽毛球':'badminton-img'
+  '篮球': 'basketball-img',
+  '足球': 'football-img',
+  '网球': 'tennis-img',
+  '排球': 'volley-img',
+  '羽毛球': 'badminton-img'
 }
 //匹配图片路径
 const imgSrc = {
-  '篮球':'/img/record/baseketball@3x.png',
-  '足球':'/img/record/volley@3x.png',
-  '网球':'/img/record/tennis@3x.png',
-  '排球':'/img/record/volley@3x.png',
-  '羽毛球':'/img/record/badminton@3x.png'
+  '篮球': baseURL + '/file/basket.png',
+  '足球': baseURL + '/file/volleyball.png',
+  '网球': baseURL + '/file/tennis.png',
+  '排球': baseURL + '/file/volleyball.png',
+  '羽毛球': baseURL + '/file/badminton.png'
 }
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-    
+
     name: {
       type: String,
       value: '未知'
@@ -50,9 +52,9 @@ Component({
    * 组件的初始数据
    */
   data: {
-    item:{
-      color:'',
-      img:''
+    item: {
+      color: '',
+      img: ''
     }
   },
 
@@ -61,19 +63,21 @@ Component({
    */
   methods: {
     //匹配当前类型
-    setClass:function(name) {
+    setClass: function (name) {
       let result = {};
-      if(colorClass[name]) console.log(222);
+      if (colorClass[name]) console.log(222);
       else console.log(111);
-      result.color = 'record-card '  + (colorClass[name] ?  colorClass[name] : 'basketball-color');
-      result.imgClass = 'img-wrap '  + (imgClass[name] ? imgClass[name] : 'basketball-img');
-      result.img = imgSrc[name] ? imgSrc[name] : '/img/record/baseketball@3x.png'
+      result.color = 'record-card ' + (colorClass[name] ? colorClass[name] : 'basketball-color');
+      result.imgClass = 'img-wrap ' + (imgClass[name] ? imgClass[name] : 'basketball-img');
+      result.img = imgSrc[name] ? imgSrc[name] : baseURL + '/file/basket.png';
       return result;
     }
   },
   lifetimes: {
     ready: function () {
-      const {name} = this.data;
+      const {
+        name
+      } = this.data;
       const item = this.setClass(name);
       this.setData({
         item
