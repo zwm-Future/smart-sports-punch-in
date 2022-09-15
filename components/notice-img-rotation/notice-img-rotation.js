d432761369ee5c89b7bfe58853d940c61a830b79
zwm-Future <871858554@qq.com>
2022-09-15 23:20:07 +0800

    轮播图动态获取

--- components/notice-img-rotation/notice-img-rotation.js
+++ components/notice-img-rotation/notice-img-rotation.js
@@ -1,23 +1,42 @@
-// components/notice-img-ratation/notice-img-ratation.js
+import {getImgNotice} from '../../api/notice'
+const app = getApp();
+const baseURL = app.globalData.baseUrl;
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
-
+    imgList:[]
   },
-
-  /**
-   * 组件的方法列表
-   */
+  lifetimes:{
+    attached:function() {
+      this._getImgNoticeList()
+    }
+  },
   methods: {
-
+    _getImgNoticeList:async function() {
+      try {
+        const {code,data} = await getImgNotice();
+        if(code) {
+          const imgList = data.map(i => baseURL + '/ann/imag?imagId=' + i.id)
+          this.setData({
+            imgList
+          })
+        }else {
+          console.log('code 0--notice-img-rotation:Component');
+        }
+      } catch (error) {
+        console.log(error,'notice-img-rotation:Component');
+      }
+    },
+    handleTapImg:function({target:{dataset:{imgIndex}}}) {
+      const {imgList} = this.data;
+      wx.previewImage({
+        urls: imgList,
+        current:imgList[imgIndex],
+        showmenu:true,
+        fail:e => {
+          console.log(e,'notice-img-rotation');
+        }
+      })
+    },
+    
   }
 })
--- components/notice-text-rotation/notice-text-rotation.js
+++ components/notice-text-rotation/notice-text-rotation.js
@@ -1,4 +1,4 @@
-// components/notice-text-rotation/notice-text-rotation.js
+import {getTextNoticeList,handleTapText} from '../../utils/notice'
 Component({
   /**
    * 组件的属性列表
@@ -11,13 +11,19 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    textList:[]
   },
-
+  lifetimes:{
+    attached:function() {
+      getTextNoticeList.call(this);
+    }
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    tapText:function(e) {
+      handleTapText.call(this,e);
+    }
   }
 })
