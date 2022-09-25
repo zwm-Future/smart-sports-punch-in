5e92e19e5f421d0a8e94f590684319e2294cc92d
zwm-Future <871858554@qq.com>
2022-09-25 17:45:35 +0800

    请求是否展示loading

--- package_teacher/api/course.js
+++ package_teacher/api/course.js
@@ -1,13 +1,13 @@
 import Request from '../../api/request';
-export const getTeacherCourse = (data) => {
+export const getTeacherCourse = (data,loading) => {
   /**获取老师的课程
   * @param {String} semesterId - 学期id
   * @param {String} teacherId - 老师id
   */
     return Request({
       url:'/teacher/course',
-      data
-    })
+      data,
+    },loading)
 }
 
 export const addTeacherCourse = (data) => {
@@ -18,5 +18,16 @@ export const addTeacherCourse = (data) => {
     return Request({
       url:`/teacher/course?semesterId=${data.semesterId}&name=${data.name}`,
       method:'POST'
+    })
+}
+
+export const deleteTeacherCourse = (data) => {
+  /**获取老师的课程
+  * @param {Array:[id]} id - 课程id
+  */
+    return Request({
+      url:"/course",
+      method:'DELETE',
+      data
     })
 }
