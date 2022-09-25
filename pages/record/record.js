d8ea1970ec788ffaf8968020575dd0fb7d4b75fb
zwm-Future <871858554@qq.com>
2022-09-25 17:43:38 +0800

    数据展示调整

--- pages/record/record.js
+++ pages/record/record.js
@@ -1,8 +1,11 @@
 import {
   getsportRecordTotal,
-  getAllsportRecord
+  getAllsportRecord,
+  getRecentlysportRecord
 } from '../../api/sports'
-import {secondTransform} from "../../utils/util"
+import {
+  secondTransform
+} from "../../utils/util"
 Page({
 
   /**
@@ -33,9 +36,10 @@ Page({
 
   },
 
-  turnSemester: function ({
+  turnSemester: async function ({
     detail: semester
   }) {
+    await this._getRecently(semester.id);
     this._getTotal(semester.id);
     this._getSportItems(semester.id);
   },
@@ -53,8 +57,11 @@ Page({
       });
       if (code) {
         console.log(data);
-        data.sportTime = data.sportTime ? secondTransform(data.sportTime) : {number:'0.0',unit:'min'};
-        data.date = data.date ? data.date : '';
+        data.sportTime = data.sportTime ? secondTransform(data.sportTime) : [{
+          number: '0.0',
+          unit: 'min'
+        }];
+        data.date = this.recentlyMost ? this.recentlyMost : '';
         this.setData({
           allSport: data
         })
@@ -64,6 +71,14 @@ Page({
     }
   },
   _getSportItems: async function (semesterId) {
+    function findDate(arr, sportId) {
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i].sportId == sportId) {
+          return arr[i].date ? arr[i].date : ' ';
+        }
+      }
+      return " ";
+    }
     try {
       const {
         stuId
@@ -75,11 +90,16 @@ Page({
         stuId,
         semesterId
       });
+      const that = this;
       if (code) {
         let recordsLeft = [];
         let recordsRight = [];
         data.forEach((item, index) => {
-          item.sportTime = item.sportTime > 0 ? secondTransform(item.sportTime) : {number:'0.0',unit:'min'};
+          item.sportTime = item.sportTime > 0 ? secondTransform(item.sportTime) : [{
+            number: '0.0',
+            unit: 'min'
+          }];
+          item.date = findDate(this.recentlyArr,item.sportId);
           index % 2 == 1 ? recordsLeft.unshift(item) : recordsRight.unshift(item)
         })
         this.setData({
@@ -95,8 +115,8 @@ Page({
               windowHeight
             } = wx.getSystemInfoSync()
             if (height < windowHeight) return;
-            const bakcTop = data.length % 2 ? 2 : 1;
-            this.setData({
+            const bakcTop = data.length % 2 ? 1 : 2;
+            that.setData({
               bakcTop
             })
           }).exec();
@@ -104,6 +124,27 @@ Page({
       }
     } catch (error) {
       console.log(error);
+    }
+  },
+  _getRecently: async function (semesterId) {
+    try {
+      const {
+        stuId
+      } = this;
+      const {
+        code,
+        data
+      } = await getRecentlysportRecord({
+        stuId,
+        semesterId
+      });
+      if (code) {
+        this.recentlyArr = data;
+        this.recentlyMost = '';
+        this.recentlyArr.forEach(i => this.recentlyMost = this.recentlyMost <= i.date ? i.date : '')
+      }
+    } catch (error) {
+      console.log(error, 'record:Page');
     }
   }
 })
