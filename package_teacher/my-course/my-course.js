7f8eda83e9c25484dc97eb1f2547be2f12be48b3
zwm-Future <871858554@qq.com>
2022-09-25 17:42:11 +0800

    当前学期获取封装，可删除课程

--- package_teacher/my-course/my-course.js
+++ package_teacher/my-course/my-course.js
@@ -1,7 +1,12 @@
 import {
   getTeacherCourse,
-  addTeacherCourse
+  addTeacherCourse,
+  deleteTeacherCourse
 } from '../api/course'
+
+import {
+  Toast
+} from "../../public/showTip"
 Page({
 
   /**
@@ -9,43 +14,32 @@ Page({
    */
   data: {
     courseList: [],
-    addModal:false
+    addModal: false,
+    deleteModal: false,
+    deleteCourse: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  onLoad:function() {
+  onLoad: function () {
     this.teacher = wx.getStorageSync('user');
-
-  },
-  onShow() {
-    this.initData()
   },
   turnSemester: function ({
     detail: semester
   }) {
+    this.semester = semester;
     this._getTeacherCourses(semester.id)
   },
-  initData: async function () {
-    this._getTeacherCourses();
-  },
-  _getTeacherCourses: async function (semesterId) {
+  _getTeacherCourses: async function (semesterId,loading) {
     try {
       let teacherId;
-      if(this.teacher.id) teacherId = this.teacher.id;
-      else throw('teacherId未知');
+      if (this.teacher.id) teacherId = this.teacher.id;
+      else throw ('teacherId未知');
       const {
         code,
         data
       } = await getTeacherCourse({
         semesterId,
         teacherId
-      });
+      },
+      loading);
       code && this.setData({
         courseList: data
       })
@@ -54,36 +48,82 @@ Page({
       console.log(error);
     }
   },
-  handleLongPress:function(e) {
-    console.log(e);
-  },
-  showAddModal:function(e) {
-    this.setData({
-      addModal:true
-    })
-  },
-  addTurnSemester:function({detail:semester}) {
-    this.addSemester = semester;
-  },
-  handleCancle:function() {
-    this.setData({
-      addModal:false
-    })
-  },
-  handleAdd:async function(e) {
-   try {
-    const {name}  = e.detail.value;
-    if(this.addSemester.id && name) {
-      const {code} = await addTeacherCourse({semesterId:this.addSemester.id,name});
-      if(code) {
-        this.handleCancle();
-      }else {
-        console.log('error');
+  handleLongPress: function ({
+    target: {
+      dataset: {
+        courseIndex,
+        id
       }
     }
-   } catch (error) {
-     console.log(error);
-   }
-
+  }) {
+    this.setData({
+      deleteModal: true,
+      addModal: false,
+      deleteCourse: this.data.courseList[courseIndex].name
+    })
+    this.deleteCourseId = id;
+  },
+  showAddModal: function (e) {
+    this.setData({
+      addModal: true,
+      deleteModal: false,
+    })
+  },
+  addTurnSemester: function ({
+    detail: semester
+  }) {
+    this.addSemester = semester;
+  },
+  handleCancle: function () {
+    this.setData({
+      addModal: false,
+      deleteModal: false
+    })
+  },
+  handleAdd: async function (e) {
+    try {
+      const {
+        name
+      } = e.detail.value;
+      if (this.addSemester.id && name) {
+        const {
+          code
+        } = await addTeacherCourse({
+          semesterId: this.addSemester.id,
+          name
+        });
+        if (code) {
+          Toast('新增成功', 'success');
+          this.handleCancle();
+        } else {
+          Toast('新增失败', 'error');
+          console.log('error');
+        }
+        this._getTeacherCourses(this.semester.id,false)
+      }else {
+        Toast('请输入课程名称','error')
+      }
+    } catch (error) {
+      this._getTeacherCourses(this.semester.id,false)
+      console.log(error, 'my-course:Page');
+    }
+  },
+  deleteCourse: async function () {
+    try {
+      const {
+        code,
+        message
+      } = await deleteTeacherCourse([this.deleteCourseId]);
+      if (code) {
+        Toast(message, 'success');
+      } else {
+        Toast('刪除失败!', 'error');
+      }
+      this.handleCancle();
+      this._getTeacherCourses(this.semester.id,false)
+    } catch (error) {
+      this._getTeacherCourses(this.semester.id,false)
+      console.log(error, 'my-course:Page');
+    }
   }
 })
